a630d2a92e43e538ebf3adb79d4d0d980eb88e2d
Jagan Raju Sakharkar <116881627+Jagansakharkar@users.noreply.github.com>
2023-06-11 19:22:33 +0530

    Add files via upload

---
+++ Count String length/script.js
@@ -0,0 +1,8 @@
+
+function countword(){
+  let input=document.getElementById('input').value
+  let output=document.getElementById("output")
+input=input.trim() 
+  output.innerHTML=input.length;
+
+}
