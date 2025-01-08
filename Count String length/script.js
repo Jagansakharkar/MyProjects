b9308dfe7947e4ac5eb9757deaf942156bd01b84
Jagan Raju Sakharkar <116881627+Jagansakharkar@users.noreply.github.com>
2025-01-08 22:36:10 +0530

    Delete Count String length directory

--- Count String length/script.js
+++
@@ -1,8 +0,0 @@
-
-function countword(){
-  let input=document.getElementById('input').value
-  let output=document.getElementById("output")
-input=input.trim() 
-  output.innerHTML=input.length;
-
-}
